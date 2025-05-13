3eafc8bd1cd46248cf590a76110bedce9974e910
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2025-05-13 20:42:44 -0300

    refactor:  indentation in handle_error function

--- src/utils/general_utils.c
+++ src/utils/general_utils.c
@@ -1,13 +1,13 @@
 #include "../../includes/minishell.h"
 
-void handle_error(char *data, char *msg, int code)
+void	handle_error(char *data, char *msg, int code)
 {
-    if (msg)
-    {
-        write(STDERR_FILENO, msg, ft_strlen(msg));
-        write(STDERR_FILENO, "\n", 1);
-    }
-    if (data)
-        free(data);
-    exit(code);
+	if (msg)
+	{
+		write(STDERR_FILENO, msg, ft_strlen(msg));
+		write(STDERR_FILENO, "\n", 1);
+	}
+	if (data)
+		free(data);
+	exit(code);
 }
