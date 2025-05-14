73adf866c36420ffd83850ecaf0e6932dc1647ed
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2025-05-13 21:05:18 -0300

    refactor: add TODO comments for manual testing in handle_error function

--- src/utils/general_utils.c
+++ src/utils/general_utils.c
@@ -2,11 +2,16 @@
 
 void	handle_error(char *data, char *msg, int code)
 {
+	// TODO: Remove comment to test manually
+	//(void)data;
+	//(void)code;
+
 	if (msg)
 	{
 		write(STDERR_FILENO, msg, ft_strlen(msg));
 		write(STDERR_FILENO, "\n", 1);
 	}
+	// TODO: Comment those lines to test manually
 	if (data)
 		free(data);
 	exit(code);
