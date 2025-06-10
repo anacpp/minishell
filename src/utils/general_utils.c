ad61de0c8306713b2c5cf1e188fed82a55fcfeaa
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2025-06-10 02:20:28 -0300

    refactor: enhance error handling in handle_error function to conditionally exit

--- src/utils/general_utils.c
+++ src/utils/general_utils.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   general_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acesar-p <acesar-p@student.42.rio>         +#+  +:+       +#+        */
+/*   By: rjacques <rjacques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 12:00:00 by acesar-p          #+#    #+#             */
-/*   Updated: 2025/05/16 20:14:57 by acesar-p         ###   ########.fr       */
+/*   Updated: 2025/06/09 01:24:22 by rjacques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* TODO: comment to test manually -> 
+/* TODO: comment to test manually ->
 (void)data;
 (void)code;
 Comment -> if(data) to exit(code) lines to test manually
@@ -21,14 +21,16 @@ Comment -> if(data) to exit(code) lines to test manually
 
 #include "../../includes/minishell.h"
 
-void	handle_error(char *data, char *msg, int code)
+void	handle_error(char *data, char *msg, int code, int should_exit)
 {
 	if (msg)
 	{
+		write(STDERR_FILENO, "minishell: ", 11);
 		write(STDERR_FILENO, msg, ft_strlen(msg));
 		write(STDERR_FILENO, "\n", 1);
 	}
 	if (data)
 		free(data);
-	exit(code);
+	if (should_exit)
+		exit(code);
 }
